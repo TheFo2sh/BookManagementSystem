d4288029bcf12b16f88f4ae9293db46818027293
Ahmed Fouad <Ahmed.Fouad.net@hotmail.com>
2021-09-20 00:06:21 +0200

    Fontend:setup data grid pagging

--- BookManagementSystem/ClientApp/src/app/home/home.component.ts
+++ BookManagementSystem/ClientApp/src/app/home/home.component.ts
@@ -1,14 +1,39 @@
-import { Component, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  page = { limit: 2,count: 1,offset: 0,orderBy: 'id',orderDir: 'desc'};
   rows:Book[]
+  http: HttpClient;
+  baseUrl: string;
+ 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Book[]>(baseUrl + 'Books').subscribe(books=>this.rows=books);
+    this.http=http;
+    this.baseUrl=baseUrl;
+  }
+ 
+  ngOnInit(): void {
+    this.pageCallback({ offset: 0 });
+  }
+
+  pageCallback(arg: { offset: number; }) {
+    this.page.offset = arg.offset;
+    this.LoadData();
+  }
+
+  LoadData() {
+    const params = new HttpParams()
+      .set('page', `${this.page.offset+1}`)
+      .set('pageSize', `${this.page.limit}`);
+
+    this.http.get<Book[]>(this.baseUrl + 'Books',{params}).subscribe(books=>{
+      this.rows=books;
+      this.page.count=10;
+    });
   }
  
   columns = [{ prop: 'id' }, { name: 'title' }, { name: 'description' }, { name: 'category' }, { name: 'authors' }];
