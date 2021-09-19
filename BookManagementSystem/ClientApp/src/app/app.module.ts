2d1b9a5fc2ac26474432dcfdc5ba559d309f5ebc
Ahmed Fouad <Ahmed.Fouad.net@hotmail.com>
2021-09-19 23:37:23 +0200

    Frontend:Setup books grid list

--- BookManagementSystem/ClientApp/src/app/app.module.ts
+++ BookManagementSystem/ClientApp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FetchDataComponent
   ],
   imports: [
+    NgxDatatableModule,
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
--- BookManagementSystem/ClientApp/src/app/home/home.component.ts
+++ BookManagementSystem/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  rows:Book[]
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Book[]>(baseUrl + 'Books').subscribe(books=>this.rows=books);
+  }
+ 
+  columns = [{ prop: 'id' }, { name: 'title' }, { name: 'description' }, { name: 'category' }, { name: 'authors' }];
+}
+interface Book{
+   id:string ;
+   title:string  ;
+   description:string  ;
+   category:string  ;
+   authors:string[]  ;
 }
